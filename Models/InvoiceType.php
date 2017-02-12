7d2caebe42e754295f7c9f263459e6c5540b2294
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-02-12 21:43:10 +0100

    Bulkl path fix reverse

---
+++ Models/InvoiceType.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.1
+ *
+ * @category   TBD
+ * @package    TBD
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://orange-management.com
+ */
+namespace Modules\Sales\Models;
+
+use phpOMS\Datatypes\Enum;
+
+/**
+ * Invoice types enum.
+ *
+ * @category   Modules
+ * @package    Sales
+ * @author     OMS Development Team <dev@oms.com>
+ * @author     Dennis Eichhorn <d.eichhorn@oms.com>
+ * @license    OMS License 1.0
+ * @link       http://orange-management.com
+ * @since      1.0.0
+ */
+abstract class InvoiceType extends Enum
+{
+    /* public */ const OFFER = 0;
+
+    /* public */ const CONTRACT_NOTE = 1;
+
+    /* public */ const DELIVERY_NOTE = 2;
+
+    /* public */ const BILL = 3;
+}
