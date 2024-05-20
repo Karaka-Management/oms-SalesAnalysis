8a46aca51990b7ac900a491bdaebec20ace309a0
Dennis Eichhorn <spl1nes.com@googlemail.com>
2024-05-21 00:09:17 +0200

    fix version and bugs

--- Controller/ApiController.php
+++ Controller/ApiController.php
@@ -6,7 +6,7 @@
  *
  * @package   Modules\SalesAnalysis
  * @copyright Dennis Eichhorn
- * @license   OMS License 2.0
+ * @license   OMS License 2.2
  * @version   1.0.0
  * @link      https://jingga.app
  */
@@ -18,7 +18,7 @@ namespace Modules\SalesAnalysis\Controller;
  * SalesAnalysis api controller class.
  *
  * @package Modules\SalesAnalysis
- * @license OMS License 2.0
+ * @license OMS License 2.2
  * @link    https://jingga.app
  * @since   1.0.0
  */
